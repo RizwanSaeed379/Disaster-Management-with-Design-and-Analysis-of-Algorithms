e904b5541b8149696e05daf95544f48ebd3062d2
RizwanSaeed379 <saeedrizwan612@gmail.com>
2025-11-23 20:30:08 +0500

    Added json file for input readin through fstream file reading

--- main.cpp
+++ main.cpp
@@ -1,37 +1,64 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include "Graph.h"
 #include "node.h"
 #include "edge.h"
 #include "vehicle.h"
 #include "DisasterManager.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
+
 
 using namespace std;
 
 int main() {
-    // Create a graph with 5 nodes: 0 = depot, 1-4 = demand nodes
-    Graph g(5);
-    g.nodes = {
-        {0, 0, 0},   // depot
-        {1, 3, 5},
-        {2, 2, 3},
-        {3, 4, 4},
-        {4, 1, 2}
-    };
 
-    // Add edges: u, v, cost, reliability
-    g.addEdge(0, 1, 4, 0.9);
-    g.addEdge(0, 2, 6, 0.8);
-    g.addEdge(1, 2, 2, 0.7);
-    g.addEdge(1, 3, 5, 0.95);
-    g.addEdge(2, 3, 3, 0.85);
-    g.addEdge(3, 4, 4, 0.9);
+    ifstream file("input.json");
+    if (!file) {
+        cerr << "Failed to open input.json\n";
+        return 1;
+    }
 
-    // Vehicles: id, capacity
-    vector<Vehicle> vehicles = {
-        {1, 5, {}}, // capacity 5
-        {2, 6, {}}, // capacity 6
-    };
+    json cfg;
+    file >> cfg;
+
+   
+    int numNodes = cfg["graph"]["num_nodes"];
+    Graph g(numNodes);
+
+    // Load nodes
+    for (auto &jn : cfg["graph"]["nodes"]) {
+        Node n;
+        n.id = jn["id"];
+        n.demand  = jn["x"];
+        n.priority  = jn["y"];
+        g.nodes.push_back(n);
+    }
+
+    // Load edges
+    for (auto &je : cfg["graph"]["edges"]) {
+        int u = je["u"];
+        int v = je["v"];
+        int cost = je["cost"];
+        double rel = je["reliability"];
+        g.addEdge(u, v, cost, rel);
+    }
+
+   
+    vector<Vehicle> vehicles;
+
+    for (auto &jv : cfg["vehicles"]) {
+        Vehicle v;
+        v.id = jv["id"];
+        v.capacity = jv["capacity"];
+        vehicles.push_back(v);
+    }
+
+
+    
+    
 
     // Create DisasterManager
     DisasterManager dm(g, vehicles);
